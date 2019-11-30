0791cb40ebe4b1456cbd6f7641bf964fa41adeea
Najeeb Hassan <najeebhassan33@gmail.com>
2019-11-30 20:25:37 +0000

    Tank Waypoints Update

--- Moving/Waypoints/Assets/WaypointFollow.cs
+++ Moving/Waypoints/Assets/WaypointFollow.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class WaypointFollow : MonoBehaviour {
 
-    public GameObject[] waypoints;
+    //public GameObject[] waypoints;
+    public UnityStandardAssets.Utility.WaypointCircuit circuit;
+
     int currentWP = 0;
 
     float speed = 3.0f;
@@ -13,21 +15,28 @@ public class WaypointFollow : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+        //waypoints = GameObject.FindGameObjectsWithTag("waypoint");
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        if (waypoints.Length == 0) return;
+        print("Waypoints found: " + circuit.Waypoints.Length);
 
-        Vector3 lookAtGoal = new Vector3(waypoints[currentWP].transform.position.x, this.transform.position.y, waypoints[currentWP].transform.position.z);
+
+        if (circuit.Waypoints.Length == 0) return;
+
+        Vector3 lookAtGoal = new Vector3(circuit.Waypoints[currentWP].position.x,
+                                                 this.transform.position.y, 
+                                                 circuit.Waypoints[currentWP].position.z);
         Vector3 direction = lookAtGoal = this.transform.position;
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                                   Quaternion.LookRotation(direction), 
+                                                   Time.deltaTime * rotSpeed);
 
         if(direction.magnitude < accuracy)
         {
             currentWP++;
-            if(currentWP >= waypoints.Length)
+            if(currentWP >= circuit.Waypoints.Length)
             {
                 currentWP = 0;
             }
